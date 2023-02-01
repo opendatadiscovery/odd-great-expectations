15faeb5386201b03bca9bd679534849518b7e8ca
Pavel Mackarichev <vixtir90@gmail.com>
2023-02-01 16:49:04 +0300

    chore: windows path debug

--- odd_great_expectations/dataset/file.py
+++ odd_great_expectations/dataset/file.py
@@ -1,11 +1,21 @@
 from pathlib import Path
 
 from great_expectations.execution_engine import PandasExecutionEngine
+from loguru import logger
 from oddrn_generator.generators import FilesystemGenerator
 
 
 def get_file_dataset(engine: PandasExecutionEngine) -> list[str]:
     batch_spec = engine.batch_manager.active_batch.batch_spec
+
+    logger.debug("Batch spec")
+    logger.debug(batch_spec.__dict__)
+    logger.debug(f"Resolved path: {Path(batch_spec['path']).resolve()}")
+
     batch_path = str(Path(batch_spec["path"]).resolve())
     generator = FilesystemGenerator(host_settings="local", path=batch_path)
-    return [generator.get_oddrn_by_path("path")]
+
+    path = generator.get_oddrn_by_path("path")
+    logger.debug(f"Resulted path: {path}")
+
+    return [path]
