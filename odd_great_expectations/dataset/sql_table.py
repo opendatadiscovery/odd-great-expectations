c9f57babf174cf7384f3e751f7abbab1ae949798
Pavel Mackarichev <vixtir90@gmail.com>
2023-07-03 09:16:00 +0300

    feat: add mssql

--- odd_great_expectations/dataset/sql_table.py
+++ odd_great_expectations/dataset/sql_table.py
@@ -5,7 +5,7 @@ from great_expectations.execution_engine.sqlalchemy_execution_engine import (
     SqlAlchemyExecutionEngine,
 )
 from loguru import logger
-from oddrn_generator import PostgresqlGenerator, SnowflakeGenerator
+from oddrn_generator import MssqlGenerator, PostgresqlGenerator, SnowflakeGenerator
 from sqlalchemy.engine import Engine
 
 
@@ -32,7 +32,21 @@ def snowflake_dataset(engine: Engine, batch_data: SqlAlchemyBatchData) -> str:
 
     generator = SnowflakeGenerator(**generator_params)
     oddrn = generator.get_oddrn_by_path("tables")
-    logger.info(f"{oddrn=}")
+    logger.info(f"Snowflake source {oddrn=}")
+    return oddrn
+
+
+def mssql_dataset(engine: Engine, batch_data: SqlAlchemyBatchData) -> str:
+    generator_params = {
+        "host_settings": engine.url.host,
+        "databases": engine.url.database,
+        "schemas": batch_data.source_schema_name or "dbo",
+        "tables": batch_data.source_table_name,
+    }
+
+    generator = MssqlGenerator(**generator_params)
+    oddrn = generator.get_oddrn_by_path("tables")
+    logger.info(f"MSSQL source {oddrn=}")
     return oddrn
 
 
@@ -45,5 +59,7 @@ def get_sql_table_dataset(exec_engine: SqlAlchemyExecutionEngine) -> List[str]:
         return [postgres_dataset(engine, batch_data)]
     elif dialect_name == "snowflake":
         return [snowflake_dataset(engine, batch_data)]
+    elif dialect_name == "mssql":
+        return [mssql_dataset(engine, batch_data)]
 
     raise ValueError(f"Unknown {dialect_name=}")
